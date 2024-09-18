0c8eadf75fd6c50a40c4fd33657e8aca77e9965e
Tarek <tarek.alhomsi@hotmail.com>
2024-09-18 23:17:38 +0200

    extra features

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -20,29 +20,30 @@ export const authOptions = {
         // Invoked on successful sign in
         async signIn({ profile }) {
             // 1. Connect to the database
-            // await connectDB();
+            await connectDB();
             // 2. Check if user exists
-            // const userExists = await User.findOne({ email: profile.email });
+            const userExists = await User.findOne({ email: profile.email });
             // 3. If not, create user
-            // if (!userExists) {
-            // Truncate username if too long
-            // const username = profile.name.slice(0, 20);
+            if (!userExists) {
+                // Truncate username if too long
+                const username = profile.name.slice(0, 20);
 
-            // await User.create({
-            // email: profile.email,
-            // username,
-            // image: profile.picture
-            // })
-            // }
+                await User.create({
+                    email: profile.email,
+                    username,
+                    image: profile.picture
+                })
+            }
             // 4. Return true to allow sign in
             return true;
         },
         // Session callback function that modifies the session object
         async session({ session }) {
+            await connectDB();
             // 1. Get user from database
-            // const user = await User.findOne({ email: session.user.email });
+            const user = await User.findOne({ email: session.user.email });
             // 2. Assign user id  from the session
-            session.user.id = '66e5eb8032411d52273a7599';
+            session.user.id = user._id.toString();
             // 3. Return session
             return session;
         },
